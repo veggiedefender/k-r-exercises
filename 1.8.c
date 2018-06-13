bdf7ca8cca68a9343aa89651fea25768fc479372
veggiedefender <jessejesse123@gmail.com>
2018-06-13 17:21:52 -0400

    1-8

---
+++ 1.8.c
@@ -0,0 +1,24 @@
+/* Write a program to count blanks, tabs, and newlines. */
+
+#include <stdio.h>
+
+main() {
+    int c, blanks, tabs, newlines;
+    blanks = tabs = newlines = 0;
+    while ((c = getchar()) != EOF) {
+        switch(c) {
+            case ' ':
+                blanks++;
+                break;
+            case '\t':
+                tabs++;
+                break;
+            case '\n':
+                newlines++;
+                break;
+        }
+    }
+    printf("blanks: %d\n", blanks);
+    printf("tabs: %d\n", tabs);
+    printf("newlines: %d\n", newlines);
+}
