5a16aa54b8b9499ce955a3eb4b0f50f66407fd5c
veggiedefender <jessejesse123@gmail.com>
2018-06-13 17:35:57 -0400

    1-10

---
+++ 1.10.c
@@ -0,0 +1,24 @@
+/* Write a program to copy its input to its output, replacing each tab by \t,
+each backspace by \b, and each backslash by \\. This makes tabs and backspaces
+visible in an unambiguous way. */
+
+#include <stdio.h>
+
+main() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        switch (c) {
+            case '\t':
+                printf("\\t");
+                break;
+            case '\b':
+                printf("\\b");
+                break;
+            case '\\':
+                printf("\\\\");
+                break;
+            default:
+                putchar(c);
+        }
+    }
+}
