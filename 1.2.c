a1ed005c61d4aea543aefa21f036d4100747f0aa
veggiedefender <jessejesse123@gmail.com>
2018-06-13 16:44:49 -0400

    1-2

---
+++ 1.2.c
@@ -0,0 +1,8 @@
+/* Experiment to find out what happens when printf's string contains \c, where c
+is some character not listed above */
+
+#include <stdio.h>
+
+main() {
+    printf("this is\roverwritten.\n");
+}
