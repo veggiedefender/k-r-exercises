0c8f89ac6279dc7d46e8cae856ca941771b6dd97
veggiedefender <jessejesse123@gmail.com>
2018-06-13 19:12:43 -0400

    1-14

---
+++ 1.14.c
@@ -0,0 +1,26 @@
+/* Write a program to print a histogram of the frequencies of different
+characters in its input. */
+
+#include <stdio.h>
+
+#define MAX_CODE 255
+
+main() {
+    int c;
+    int freqs[MAX_CODE + 1];
+    for (int i = 0; i <= MAX_CODE; i++) {
+        freqs[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        freqs[c]++;
+    }
+
+    for (int i = 0; i <= MAX_CODE; i++) {
+        printf("%d\t|", i);
+        for (int j = 0; j < freqs[i]; j++) {
+            printf("X");
+        }
+        printf("\n");
+    }
+}
