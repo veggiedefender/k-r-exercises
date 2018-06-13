3a30267e0b34f76223fd19b902351a592d3fd0ea
veggiedefender <jessejesse123@gmail.com>
2018-06-13 16:51:44 -0400

    1-3

---
+++ 1.3.c
@@ -0,0 +1,21 @@
+/* Modify the temperature conversion program to print a heading above the
+table. */
+
+#include <stdio.h>
+
+main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("F\tC\n");
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
