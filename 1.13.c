c0607905235d5eb269d53152aa4e9878d4f88419
veggiedefender <jessejesse123@gmail.com>
2018-06-13 18:54:46 -0400

    1-13

---
+++ 1.13.c
@@ -0,0 +1,39 @@
+/* Write a program that prints its input one word per line. */
+
+#include <stdio.h>
+
+#define IN       1
+#define OUT      0
+#define LONGEST  9
+
+main() {
+    int c, state, length;
+    int lengths[LONGEST + 1];
+    length = 0;
+    state = OUT;
+    for (int i = 0; i <= LONGEST; i++) {
+        lengths[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n' || c == ' ' || c == '\t') {
+            if (length > LONGEST) {
+                length = LONGEST;
+            }
+            lengths[length]++;
+            length = 0;
+            state = OUT;
+        } else {
+            length++;
+            state = IN;
+        }
+    }
+
+    for (int i = 0; i <= LONGEST; i++) {
+        printf("%d | ", i);
+        for (int j = 0; j < lengths[i]; j++) {
+            printf("X");
+        }
+        printf("\n");
+    }
+}
